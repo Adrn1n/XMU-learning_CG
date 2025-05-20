ecae7437ddda0dc1388d9eb79f55592bf47e628b
Exxon <exxonliu001@yahoo.com>
2025-05-20 15:43:55 +0800

    20250520: Finish: 20250507/Code

--- Code/20250507/src/main.cpp
+++ Code/20250507/src/main.cpp
@@ -14,16 +14,19 @@ using namespace std;
 float toRadians(float degrees) { return (degrees * 2.0f * 3.14159f) / 360.0f; }
 
 #define numVAOs 1
-#define numVBOs 3
+// #define numVBOs 3
+#define numVBOs 4 //
 
 float cameraX, cameraY, cameraZ;
 float gndLocX, gndLocY, gndLocZ;
-GLuint renderingProgram;
+// GLuint renderingProgram;
+GLuint renderingProgram, renderingProgramCubeMap; //
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 
 // variable allocation for display
-GLuint mvLoc, projLoc;
+// GLuint mvLoc, projLoc;
+GLuint mvLoc, projLoc, vLoc; //
 int width, height;
 float aspect;
 glm::mat4 pMat, vMat, mMat, mvMat;
@@ -34,9 +37,26 @@ int numGroundVertices;
 
 GLuint heightMap;
 GLuint heightTexture;
+GLuint skyboxTexture; //
 
 void setupVertices(void)
 {
+	/*
+	 */
+	float cubeVertexPositions[108] =
+		{-1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
+		 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
+		 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+		 -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+		 -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+		 -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+		 -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,
+		 -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f};
+
 	numGroundVertices = ground.getNumVertices();
 	std::vector<glm::vec3> vert = ground.getVertices();
 	std::vector<glm::vec2> tex = ground.getTextureCoords();
@@ -70,12 +90,18 @@ void setupVertices(void)
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
 	glBufferData(GL_ARRAY_BUFFER, nvalues.size() * 4, &nvalues[0], GL_STATIC_DRAW);
+
+	/*
+	 */
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[3]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertexPositions), cubeVertexPositions, GL_STATIC_DRAW);
 }
 
 void init(GLFWwindow *window)
 {
 	// renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
-	renderingProgram = Utils::createShaderProgram("assets/shaders/vertShader.glsl", "assets/shaders/fragShader.glsl"); //
+	renderingProgram = Utils::createShaderProgram("assets/shaders/vertShader.glsl", "assets/shaders/fragShader.glsl");			//
+	renderingProgramCubeMap = Utils::createShaderProgram("assets/shaders/vertCShader.glsl", "assets/shaders/fragCShader.glsl"); //
 	cameraX = 0.03f;
 	cameraY = 0.03f;
 	cameraZ = 0.8f;
@@ -95,6 +121,7 @@ void init(GLFWwindow *window)
 	// heightMap = Utils::loadTexture("assets/images/height.jpg");			   //
 	heightTexture = Utils::loadTexture("assets/images/rocky_terrain_diff_2k.png"); //
 	heightMap = Utils::loadTexture("assets/images/rocky_terrain_disp_2k_rgb.png"); //
+	skyboxTexture = Utils::loadCubeMap("assets/images/cubeMap");				   //
 }
 
 void display(GLFWwindow *window, double currentTime)
@@ -102,6 +129,33 @@ void display(GLFWwindow *window, double currentTime)
 	glClear(GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	/*
+	 */
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(gndLocX, gndLocY, gndLocZ));
+	mMat = glm::rotate(mMat, toRadians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	mvMat = vMat * mMat;
+
+	/*
+	 */
+	glUseProgram(renderingProgramCubeMap);
+	vLoc = glGetUniformLocation(renderingProgramCubeMap, "v_matrix");
+	glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
+	projLoc = glGetUniformLocation(renderingProgramCubeMap, "proj_matrix");
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+	// glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[3]); //
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, skyboxTexture);
+	glUniform1i(glGetUniformLocation(renderingProgramCubeMap, "samp"), 0); //
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CCW); // cube is CW, but we are viewing the inside
+	glDisable(GL_DEPTH_TEST);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glEnable(GL_DEPTH_TEST);
 
 	glUseProgram(renderingProgram);
 
@@ -113,10 +167,10 @@ void display(GLFWwindow *window, double currentTime)
 	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
 	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
 
-	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
-	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(gndLocX, gndLocY, gndLocZ));
-	mMat = glm::rotate(mMat, toRadians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	mvMat = vMat * mMat;
+	// vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	// mMat = glm::translate(glm::mat4(1.0f), glm::vec3(gndLocX, gndLocY, gndLocZ));
+	// mMat = glm::rotate(mMat, toRadians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	// mvMat = vMat * mMat;
 
 	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
